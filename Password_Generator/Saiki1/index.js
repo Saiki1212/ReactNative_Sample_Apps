7266c6b882b11017ed49ab81a709ac4e8e38d8eb
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2023-09-22 01:24:49 +0530

    Delete Password_Generator directory

--- Password_Generator/Saiki1/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App1 from './App1';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App1);
